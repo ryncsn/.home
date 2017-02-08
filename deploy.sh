a573d4b176ae2ec2a8b432bdeb48c34d99a358b5
ryncsn <ryncsn@gmail.com>
2017-02-08 23:18:20 +0800

    Let the script check for deps, install more components

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-PWD=$(pwd)
+_error() {
+    echo $1; exit 1;
+}
 
-if [[ ! $PWD == */.home ]]; then
-    echo "This is script is only used for fast deploy of my own env files"
-    exit 1
-fi
+_checkPwd(){
+    if [[ ! $(pwd) == */.home ]]; then
+        _error "This is script is only used for fast deploy of my own env files"
+    fi
+}
 
-if [[ ! -d ~/.oh-my-zsh ]]; then
-    echo "Installing Oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-    echo "Oh-my-zsh already installed."
-fi
-
-if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
-    echo "Installing Theme..."
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-fi
+_checkTools(){
+    for i in "git" "curl" "vim" "sed" "sh" "realpath" "dirname"; do
+        $(which $i &> /dev/null) || _error "'$i' is needed but not installed"
+    done
+}
 
 _symLink(){
     _path="$1"
@@ -33,6 +30,33 @@ _symLink(){
     fi
 }
 
-for i in $(find -type f -not -path "./deploy.sh" -not -path "./.git/*"); do
-    _symLink $i
-done
+_Install(){
+    if [[ ! -d ~/.oh-my-zsh ]]; then
+        echo "Installing Oh-my-zsh..."
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) sed 's/^env zsh$/d'"
+    fi
+
+    if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
+        echo "Installing Theme..."
+        git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    fi
+
+    if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+        echo "Installing Theme..."
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    fi
+}
+
+main(){
+    _checkPwd
+    _checkTools
+    _Install
+    for i in $(find -type f -not -path "./deploy.sh" -not -path "./.git/*"); do
+        _symLink $i
+    done
+}
+
+main
+
+#TODO
+vim -c "PluginInstall"
