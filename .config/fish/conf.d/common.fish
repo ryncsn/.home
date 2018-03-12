e5256da9459978298c25a236e8cd43bd1b9cdbf5
Kairui Song <kasong@redhat.com>
2018-03-12 14:23:18 +0800

    Add GOPATH

--- .config/fish/conf.d/common.fish
+++ .config/fish/conf.d/common.fish
@@ -6,5 +6,11 @@ alias la "ls -Gla"
 # List only directories
 alias lsd 'ls -l | grep "^d"'
 
+alias ls 'ls --color="auto"'
 alias ll 'ls -ahlF'
 alias l 'ls -CF'
+
+set -x -g GOPATH $HOME/go
+set -x -g GOBIN $GOPATH/bin
+
+set -x -g PATH $GOBIN $PATH
