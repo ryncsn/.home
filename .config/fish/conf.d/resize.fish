b5da3285b55d7d0382c8f93736ae00269ee49ae8
Kairui Song <ryncsn@gmail.com>
2023-03-06 16:09:04 +0800

    Add resize wrapper

---
+++ .config/fish/conf.d/resize.fish
@@ -0,0 +1,9 @@
+if test (type -q resize) = file
+	function resize --wraps=resize --inherit-variable resize
+		for line in (resize | grep =)
+			set item (string split -m 1 '=' $line)
+			set -gx $item[1] $item[2]
+		end
+	end
+	isatty 1 && resize
+end
