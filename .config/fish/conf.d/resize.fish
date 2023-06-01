2f1fa107a5c6c4f0589cffffb8cdc7f6484dfec3
Kairui Song <ryncsn@gmail.com>
2023-06-01 10:52:23 +0800

    Update git diff and console resize

--- .config/fish/conf.d/resize.fish
+++ .config/fish/conf.d/resize.fish
@@ -1,6 +1,7 @@
-if test "(type -t resize)" = "file"
+if test "$(type -t resize 2>/dev/null)" = "file"
+	set _resize (which resize)
 	function resize --wraps=resize --inherit-variable resize
-		for line in (resize | grep =)
+		for line in ($_resize | grep =)
 			set item (string split -m 1 '=' $line)
 			set -gx $item[1] $item[2]
 		end
