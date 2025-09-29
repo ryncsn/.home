15843c2e70753112493f59611411a1bfcd8f8e0e
Kairui Song <ryncsn@gmail.com>
2025-09-29 15:00:13 +0800

    git: improve keyword replace

--- .config/fish/conf.d/50-git.fish
+++ .config/fish/conf.d/50-git.fish
@@ -9,5 +9,5 @@ alias gicommit "git commit"
 alias gifetch "git fetch"
 
 function girename
-    sed -ie "s/\b$argv[1]\b/$argv[2]/g" (git grep -w $argv[1] | cut -d ":" -f 1 | sort -u)
+    sed -ie "s/\b$argv[1]\b/$argv[2]/g" (git grep -w "$argv[1]" | cut -d ":" -f 1 | sort -u)
 end
