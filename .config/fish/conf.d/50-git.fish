beb008cdc79085e3446e69a803477c864dafc5c7
Kairui Song <ryncsn@gmail.com>
2025-12-28 19:32:38 +0800

    add more git alias

--- .config/fish/conf.d/50-git.fish
+++ .config/fish/conf.d/50-git.fish
@@ -1,12 +1,15 @@
 alias gilog "git log"
 alias gidiff "git diff"
 alias girebase "git rebase"
+alias gishow "git show"
 alias gigrep "git grep"
 alias gicheckout "git checkout"
 alias gicherry-pick "git cherry-pick"
 alias gibranch "git branch"
+alias giblame "git blame"
 alias gicommit "git commit"
 alias gifetch "git fetch"
+alias gireset "git reset"
 
 function girename
     sed -ie "s/\b$argv[1]\b/$argv[2]/g" (git grep -w "$argv[1]" | cut -d ":" -f 1 | sort -u)
